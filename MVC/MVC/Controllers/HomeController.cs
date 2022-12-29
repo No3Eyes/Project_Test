bc0e1bba63a7bc156979920a8fc5915029c16734
ABAi <80210850+ABai-fefeder@users.noreply.github.com>
2022-12-30 00:56:24 +0800

    MVC介面

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult product()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+        public IActionResult Layout()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult orderCustomizedCake()
+        {
+            return View();
+        }
+
+        public IActionResult Class()
+        {
+            return View();
+        }
+
+        public IActionResult Class2()
+        {
+            return View();
+        }
+
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
